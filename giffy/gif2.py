ee5f260c71c15cba6c7b3265c09dbd9e6b6494a4
bunniesus <shravaniid28@gmail.com>
2026-07-23 21:51:48 +0530

    Add GIF animation (using PIL) project

---
+++ giffy/gif2.py
@@ -0,0 +1,21 @@
+from PIL import Image
+
+frames = []
+
+for i in range(37):
+    frames.append(Image.open(f"frame_{i:02d}_delay-0.1s.png"))
+
+frames[0].save(
+    "dancingcroc.gif",
+    save_all=True,
+    append_images=frames[1:],
+    duration=100,
+    loop=0,
+    disposal=2
+)
+print("Gif created successfully ! Check dancingcroc.gif")
+
+# import numpy as np
+# img1 = iio.imread("frame_00_delay-0.1s.png")
+# img2 = iio.imread("frame_01_delay-0.1s.png")
+# print(np.array_equal(img1, img2))
