6e2a5d76556d2e73e5563c2559bba28a1753b3b7
bunniesus <shravaniid28@gmail.com>
2026-07-26 14:45:33 +0530

    feat: implement Assignment Tracker CLI

--- student-assignment_tracker/assignment_tracker.py
+++ student-assignment_tracker/assignment_tracker.py
@@ -1,15 +1,14 @@
-
-
-print("---Assignment Tracker----\n"
-    "1. Add Assignment \n"
-    "2. View Assignment \n"
-    "3. Mark Complete \n"
-    "4. Exit \n"
-)
+assignments = []
+def PrintMenu():
+    print("\n---Assignment Tracker----\n"
+        "1. Add Assignment \n"
+        "2. View Assignment \n"
+        "3. Mark Complete \n"
+        "4. Exit \n"
+    )
 
 def AddAssignment():
-    
-    assignments = []
+    assignment = []
 
     subject = input("Enter Subject: ")
     title = input("Title: ")
@@ -24,20 +23,60 @@ def AddAssignment():
     "Status" : "Pending"
 }
     assignments.append(assignment)
-    
-option = int(input("Select an option : "))
 
-if option == 1:
-    AddAssignment()
+def ViewAssignment():
 
-elif option == 2:
-    print("View Assignment")
-elif option == 3:
-    print("Mark Copleted")
-elif option == 4:
-    print("Exiting Tracker ...")
-else:
-    print("Invalid Option ..")
+    if not assignments:
+        print("No assignments..")
+    else:
+        for index, assignment in enumerate(assignments, start=1):
+            print(f"{index} .")
+            print(f"Subject:  {assignment['Subject']}")
+            print(f"Title:    {assignment['Title']}")
+            print(f"Due Date: {assignment['Due Date']}")
+            print(f"Priority: {assignment['Priority']}")
+            print(f"Status:   {assignment['Status']}")
+            print("----------------------")
+
+def MarkComplete():
+    print("Current assignments ---")
+
+    if not assignments:
+            print("No assignments..")
+            return
+    ViewAssignment()
+    number = int(input("Enter assignment no: "))
+    index = number - 1
+
+    if 0<= index < len(assignments):
+        assignments[index]["Status"] = "Completed"
+        print("✅ Assignment marked as completed successfully!")
+    else:
+        print("❌ Invalid assignment number.")
+
+
+       
+
+            
+
+while True:
+    PrintMenu()
+    option = int(input("Select an option : "))
+
+    if option == 1:
+        AddAssignment()
+
+    elif option == 2:
+        ViewAssignment()
+
+    elif option == 3:
+        MarkComplete()
+
+    elif option == 4:
+        print("Exiting Tracker ...\n")
+        break
+    else:
+        print("Invalid Option ..")
 
 #Try Switcher
 
